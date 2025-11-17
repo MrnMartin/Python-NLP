3dcc90c1c1463a0d4a9af69731325d7c1b733400
MrnMartin <151888353+MrnMartin@users.noreply.github.com>
2025-11-17 11:50:36 +0100

    Delete PyNLP_Training directory

--- PyNLP_Training/Base Python/Exercice3.py
+++
@@ -1,8 +0,0 @@
-#Exercice 3 : Vérifier si un nombre est pair ou impair.
-
-Nombre = int(input("Nombre entier : "))
-
-if Nombre % 2 == 0:
-    print("Le nombre est paire")
-else:
-    print("Le nombre est impair")
--- PyNLP_Training/Base Python/Exercice5.py
+++
@@ -1,8 +0,0 @@
-#Exercice 5 : Créer une fonction qui renvoie le carré d’un nombre.
-
-#Solution 1 : 
-def Carre(n):
-    return n*n
-
-nombre = int(input("Nombre : "))
-print("Le carré est :" , Carre(nombre))
